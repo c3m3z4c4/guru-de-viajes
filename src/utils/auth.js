ad3038655961b239ad23b2fc6578fb04270189aa
C3M3Z4 <contacto@cesarmeza.com>
2022-08-03 21:26:28 -0500

    Auth process working on middleware

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,33 +1,51 @@
 const axios = require("axios");
-const FormData = require("form-data");
+// const FormData = require("form-data");
 
 const auth = async () => {
+  let urlencoded = new URLSearchParams();
+
+  urlencoded.append("grant_type", "client_credentials");
+  urlencoded.append("client_id", process.env.client_id);
+  urlencoded.append("client_secret", process.env.client_secret);
+
+  // let headers = {
+  //   "Content-Type": "application/x-www-form-urlencoded",
+  // };
+  // let body = {
+  //   grant_type: "client_credentials",
+  //   client_id: process.env.client_id,
+  //   client_secret: process.env.client_secret,
+  // };
+  // const CLIENT_ID = process.env.client_id;
+  // const CLIENT_SECRET = process.env.client_secret;
   // let form = new FormData();
   // form.append("grant_type", "client_credentials");
   // form.append("client_id", process.env.client_id);
   // form.append("client_secret", process.env.client_secret);
 
+  const uriAuth = "https://test.api.amadeus.com/v1/security/oauth2/token";
+
+  // const bodyFull = `grant_type=client_credentials&client_id=${body.client_id}&client_secret=${body.client_secret}`;
+
   // form.append(
-  //   "form",
+  //   "data",
   //   `{
-  //   grant_type: "client_credentials",
+  //   grant_type: "client_credentials"
+  // }`
+  // );
+  // form.append(
+  //   "auth",
+  //   `{
   //   client_id: ${process.env.client_id},
   //   client_secret: ${process.env.client_secret},
   // }`
   // );
 
-  let response = await axios
-    .post(
-      `https://test.api.amadeus.com/v1/security/oauth2/token?grant_type="client_credentials"&client_id=${process.env.client_id}&client_secret=${process.env.client_secret}`,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    )
-    .then((res) => {
-      return res.data;
-    });
+  console.log(`Este es el pss: ${urlencoded}`);
+
+  let response = await axios.post(uriAuth, urlencoded).then((res) => {
+    return res.data;
+  });
   return response;
 };
 
